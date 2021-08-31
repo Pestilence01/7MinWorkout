99eccc185531da75913f0abbd17a52885fbf8c58
Lovre <lovre.nin.zd@gmail.com>
2021-08-31 04:40:46 +0200

    Final project

---
+++ app/src/main/java/com/example/a7minworkout/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.a7minworkout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        llStart.setOnClickListener{
+            startActivity(Intent(this, ExerciseActivity::class.java))
+        }
+
+        llBMI.setOnClickListener{
+            startActivity(Intent(this, BMIActivity::class.java))
+        }
+
+        llHistory.setOnClickListener{
+            startActivity(Intent(this, HistoryActivity::class.java))
+        }
+    }
+}
